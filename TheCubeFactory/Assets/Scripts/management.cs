fa05b6576829756bd4bf76a5aff07c8c171ec6f5
batuqy <38671054+batuqy@users.noreply.github.com>
2023-08-03 15:22:22 +0300

    a

--- TheCubeFactory/Assets/Scripts/management.cs
+++ TheCubeFactory/Assets/Scripts/management.cs
@@ -6,16 +6,38 @@ public class management : MonoBehaviour
 {
 
     public TextMeshProUGUI costText; // Reference to the TextMeshProUGUI component.
+    private Color greenColor = Color.green;
+    private Color yellowColor = Color.yellow;
+    private Color redColor = Color.red;
 
-   
-   
+    public Slider slider;
+    public Image sliderFill;
+    private void Awake()
+    {
+        // Attach the OnValueChanged listener to the slider
+        slider.onValueChanged.AddListener(SetCostText);
+    }
     public void SetCostText(float value)
     {
-        Debug.Log("cost: " + value);
-        costText.text = value.ToString();
-     
+        
+        costText.text = "Cost:"+value.ToString();
+
+        if (value >= 1 && value <= 32)
+        {
+            sliderFill.color = greenColor;
+        }
+        else if (value >= 33 && value <= 49)
+        {
+            sliderFill.color = yellowColor;
+        }
+        else if (value >= 50 && value <= 100)
+        {
+            sliderFill.color = redColor;
+        }
+
     }
-    
+   
+
 }
 
 
